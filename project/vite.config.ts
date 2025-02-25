f875291b9454f04d9b380facac468a6a3b9ecb22
Jatinverma9728 <somveerparjapati123456@gmail.com>
2025-02-25 16:05:21 +0530

    u2

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
   optimizeDeps: {
+    include: ['framer-motion'],
     exclude: ['lucide-react'],
   },
 });
